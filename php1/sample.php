fdf2038efb654d31e13548f72dd45062cc20e92c
平石匡 <masahiraishi22@gmail.com>
2018-06-24 13:52:31 +0900

    追加：p046(phpプログラミングの教科書)

---
+++ php1/sample.php
@@ -0,0 +1,10 @@
+<?php
+
+print ('<h2 style="font-size:110%; fonnt-weight:;bold">画面にHTMLを表示する</h2>');
+print ('<p>PHPはHTMLの中に埋め込んで記述することができる。</p>');
+
+print('<p>改行するときは、<br />brタグを記述します');
+?>
+<br/>
+<?php
+print(date('s'));
