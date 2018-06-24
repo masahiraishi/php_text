3a9abf40ef920281e243d09b61688a93bf9a919e
平石匡 <masahiraishi22@gmail.com>
2018-06-24 14:27:47 +0900

    追加：p052(php_text)

--- php1/sample.php
+++ php1/sample.php
@@ -1,10 +1,5 @@
-<?php
-
-print ('<h2 style="font-size:110%; fonnt-weight:;bold">画面にHTMLを表示する</h2>');
-print ('<p>PHPはHTMLの中に埋め込んで記述することができる。</p>');
-
-print('<p>改行するときは、<br />brタグを記述します');
-?>
-<br/>
-<?php
-print(date('s'));
+<form action="sample04.php" method="get">
+	<label for="my_name">お名前：</label>
+	<input id="my_name" type="text" name="my_name" size="35" maxlength="255" value="">
+	<input type="submit" value="送信する">
+</form>
