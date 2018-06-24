8827e38b40974dfffbbd3b1b163e8bc8e3663feb
平石匡 <masahiraishi22@gmail.com>
2018-06-24 23:18:13 +0900

    追加：p55

--- php1/sample.php
+++ php1/sample.php
@@ -1,5 +1,13 @@
-<form action="sample04.php" method="get">
-	<label for="my_name">お名前：</label>
-	<input id="my_name" type="text" name="my_name" size="35" maxlength="255" value="">
-	<input type="submit" value="送信する">
-</form>
+<form action ="practice4.php" method = "get">
+	<dl>
+		<dt><label for="my_name">お名前：</label></dt>
+		<dd>
+			<input id = "my_name" type = "text" name = "my_name" size="35" maxlength = "255" value ="">
+		</dd>
+		<dt>
+			<label for = "message">メッセージ:</label>
+		</dt>
+		<dd>
+  <a href =
+			<input type =
+			　
