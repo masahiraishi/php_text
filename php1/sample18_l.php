999280977d6f7f41956594a97cc59c7ae40b30cb
平石匡 <masahiraishi22@gmail.com>
2018-07-01 09:19:14 +0900

    セッションコミット

---
+++ php1/sample18_l.php
@@ -0,0 +1,12 @@
+<?php
+
+session_start();
+
+
+
+session_unset();
+
+header('Location: sample18_input.php');
+exit;
+
+?>
---
+++ php1/sample18_s.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+?>
+
+<p><?php echo $_SESSION['my_id']; ?>さんの情報はまだ残っています </p>
+
+<p> <a href= "./sample18_l.php">ログアウトすること></a></p>
